c45f55556107f48ceb861d5866c2051bf7c50473
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-12-07 19:15:03 +0300

    Initialized project base

---
+++ src/app/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { appStore } from "./store/appStore";
+import App from "./layout/App";
+import "./style/App.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Provider store={appStore}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
