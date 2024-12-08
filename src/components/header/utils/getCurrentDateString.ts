601767ae69ae279d99c1645932d09d450af8a8e5
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-12-08 20:17:00 +0300

    Created Header component

---
+++ src/components/header/utils/getCurrentDateString.ts
@@ -0,0 +1,12 @@
+export const getCurrentDateString = () => {
+  const date = new Date();
+
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  };
+
+  return date.toLocaleDateString("en-US", options);
+};
