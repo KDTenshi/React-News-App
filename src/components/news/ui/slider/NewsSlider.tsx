fac2f9f7ee708503a8f77fd492c3c69a05de3f67
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-12-10 16:31:08 +0300

    Moved slider to UI Kit

--- src/components/news/ui/slider/NewsSlider.tsx
+++ src/components/news/ui/slider/NewsSlider.tsx
@@ -1,49 +1,24 @@
 import { FC } from "react";
 import { INews } from "../../../../shared/types/types";
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { SwiperSlide } from "swiper/react";
 import NewsCard from "../card/NewsCard";
 import "swiper/swiper-bundle.css";
 import style from "./NewsSlider.module.css";
-import { Button } from "../../../../shared/ui";
+import Slider from "../../../../shared/ui/slider/Slider";
 
 interface NewsSliderProps {
   news: INews[];
 }
 
-interface SliderButtonProps {
-  direction: "left" | "right";
-}
-
-const SliderButton: FC<SliderButtonProps> = ({ direction }) => {
-  const swiper = useSwiper();
-  const arrow = direction === "left" ? "<" : ">";
-
-  const handleClick = () => {
-    if (direction === "left") {
-      swiper.slidePrev();
-    } else {
-      swiper.slideNext();
-    }
-  };
-
-  return (
-    <Button onClick={handleClick} className={direction === "left" ? style.Button_Left : style.Button_Right}>
-      {arrow}
-    </Button>
-  );
-};
-
 const NewsSlider: FC<NewsSliderProps> = ({ news }) => {
   return (
-    <Swiper slidesPerView={1} className={style.NewsSlider}>
-      <SliderButton direction="left" />
+    <Slider>
       {news.map((item) => (
         <SwiperSlide key={item.article_id} className={style.Slide}>
           <NewsCard item={item} size="big" />
         </SwiperSlide>
       ))}
-      <SliderButton direction="right" />
-    </Swiper>
+    </Slider>
   );
 };
 
