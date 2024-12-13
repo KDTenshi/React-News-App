f80cc59103501c62ec782fe000d983a6ef01588b
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-12-13 16:44:24 +0300

    Created NewsSlider component

---
+++ src/components/news/ui/slider/NewsSlider.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { Slider } from "../../../../shared/ui";
+import { useGetLatestNewsQuery } from "../../../../shared/api/newsApi";
+import NewsCard from "../card/NewsCard";
+
+const NewsSlider: FC = () => {
+  const { data } = useGetLatestNewsQuery();
+
+  return <Slider loop>{data && data.news.map((item) => <NewsCard key={item.id} newsItem={item} />)}</Slider>;
+};
+
+export default NewsSlider;
